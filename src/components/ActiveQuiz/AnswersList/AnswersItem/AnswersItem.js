26f40916210269bb38a72b36dcf0bb7d9c6b0e87
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-11-03 23:00:11 +0300

    added stuff

---
+++ src/components/ActiveQuiz/AnswersList/AnswersItem/AnswersItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from '../AnswersItem/AnswersItem.module.css'
+
+const AnswerItem = props => {
+    return (
+        <li className={classes.AnswerItem}>
+            { props.answer.text }
+        </li>
+    )
+}
+
+export default AnswerItem
