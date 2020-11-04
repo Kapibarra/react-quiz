5a5ff0b973e878c9560944d1231cdd2f3e5db593
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-11-04 19:37:04 +0300

    components

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,20 +1,24 @@
 import React from "react";
 import classes from "./ActiveQuiz.module.css";
-import AnswersList from './AnswersList/AnswersList'
+import AnswersList from "./AnswersList/AnswersList";
 
 const ActiveQuiz = (props) => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
-        <strong>2.</strong>&nbsp;
-        How are you?
+        <strong>{props.answerNumber}.</strong>&nbsp;
+        {props.question}
       </span>
-      <small>4 из 12</small>
+      <small>
+        {props.answerNumber} из {props.quizLength}
+      </small>
     </p>
 
-  <AnswersList 
+    <AnswersList
+      state={props.state}
       answers={props.answers}
-  />
+      onAnswerClick={props.onAnswerClick}
+    />
   </div>
 );
 
--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -9,6 +9,8 @@ return (
     <AnswerItem 
     key={index}
         answer={answer}
+        onAnswerClick={props.onAnswerClick}
+        state={props.state ? props.state[answer.id] : null}
     />
 )
     })}
