0c01e191a9772edfeff4a3202aa8c74a991aca40
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-01-04 18:22:34 +0300

    fix

--- src/components/Navigation/MenuToggle/MenuToggle.js
+++ src/components/Navigation/MenuToggle/MenuToggle.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import classes from './MenuToggle.module.css'
 
-const MenuToggle = props => {
+
+
+const MenuToggle = (props) => {
   const cls = [
     classes.MenuToggle,
     'fa'
